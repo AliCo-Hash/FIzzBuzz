a239ec8e012f78fdf88131d5cd2d3ee5e3b29462
CBuchan <ec2-user@ip-172-31-26-241.eu-west-1.compute.internal>
2022-01-18 16:34:16 +0000

    updated files

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,17 @@
 def fizzbuzz(number)
+ 1.upto 20 do |n| 
+  if n % 15 == 0 
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  
+  elsif n % 5 == 0
+    puts "Buzz"
+  
+  else
+    puts n
+    
+      
+  end
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,22 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end 
 end 
+
+describe 'fizzbuzz' do 
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+end 
+
+describe 'fizzbuzz' do 
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end 
+end     
+
+# describe 'fizzbuzz' do 
+#     it 'returns number' do
+#         expect(fizzbuzz(15)).to eq 'fizzbuzz'
+#   end 
+# end  
+
+end
